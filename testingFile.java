821192a5db047b42c78dd3e328fff43b580e2382
jeewanMgr <jeewanmgr29@gmail.com>
2018-06-06 16:58:17 -0400

    Create testingFile.java

---
+++ testingFile.java
@@ -0,0 +1,11 @@
+
+public class testingFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            Encryption happy = new Encryption();
+            happy.BigMan();
+            		
+	}
+
+}
